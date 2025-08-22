4cee00d736491da7a2da99b69ccd0eb2317635c9
Andezion <245122@edu.p.lodz.pl>
2025-08-22 23:33:54 +0200

    need to fix list and array

--- list.h
+++ list.h
@@ -1,189 +1,86 @@
 #pragma once
-#include <list>
 #include <stdexcept>
 
 template<typename T>
-class list
-{
-    T * storage;
-    size_t size_of_list{};
+class list {
+    struct Node {
+        T value;
+        Node* prev;
+        Node* next;
+        explicit Node(const T& v, Node* p=nullptr, Node* n=nullptr)
+            : value(v), prev(p), next(n) {}
+    };
+
+    Node* head;
+    Node* tail;
+    size_t size_of_list;
+
 public:
-    list();
-    explicit list(size_t size);
-    list(const T & value, size_t size);
-    list(const list& other);
-    list(list&& other) noexcept;
-    list(T* begin, T* end);
-    list(std::initializer_list<T> init);
-
-    void assign(size_t size, const T & value);
-    T& back() const;
-    T* begin() const;
-    const T* cbegin() const noexcept;
-    const T* cend() const noexcept;
-    void clear();
-    const T& crbegin() const noexcept;
-    const T* crend() const noexcept;
-    T emplace(size_t pos, const T & value);
-
-    ~list();
-};
-
-template<typename T>
-list<T>::list()
-{
-    storage = nullptr;
-    size_of_list = 0;
-}
-
-template<typename T>
-list<T>::list(const size_t size)
-{
-    storage = new T[size];
-    size_of_list = size;
-}
-
-template<typename T>
-list<T>::list(const T &value, size_t size)
-{
-    storage = new T[size];
-    size_of_list = size;
-
-    for (size_t i = 0; i < size; i++)
-    {
-        storage[i] = value;
+    list() : head(nullptr), tail(nullptr), size_of_list(0) {}
+    list(const list& other) : head(nullptr), tail(nullptr), size_of_list(0) {
+        for (Node* p = other.head; p; p = p->next) push_back(p->value);
     }
-}
-
-
-template<typename T>
-list<T>::list(const list<T> &other)
-{
-    storage = new T[other.size_of_list];
-    size_of_list = other.size_of_list;
-
-    for (size_t i = 0; i < size_of_list; i++)
-    {
-        storage[i] = other.storage[i];
+    list(list&& other) noexcept : head(other.head), tail(other.tail), size_of_list(other.size_of_list) {
+        other.head = other.tail = nullptr;
+        other.size_of_list = 0;
     }
-}
-
-template<typename T>
-list<T>::list(list &&other) noexcept
-{
-    storage = other.storage;
-    size_of_list = other.size_of_list;
-
-    delete[] other.storage;
-    other.storage = nullptr;
-    other.size_of_list = 0;
-}
-
-template<typename T>
-list<T>::list(T *begin, T *end)
-{
-    storage = new T[end - begin];
-    size_of_list = end - begin;
-    for (size_t i = 0; i < size_of_list; i++)
-    {
-        storage[i] = begin[i];
+    list(std::initializer_list<T> init) : head(nullptr), tail(nullptr), size_of_list(0) {
+        for (auto& v : init) push_back(v);
     }
-}
+    ~list() { clear(); }
 
-template<typename T>
-list<T>::list(std::initializer_list<T> init)
-{
-    storage = new T[init.size()];
-    size_of_list = init.size();
-    for (size_t i = 0; i < size_of_list; i++)
-    {
-        storage[i] = init[i];
+    void push_back(const T& value) {
+        Node* node = new Node(value, tail, nullptr);
+        if (tail) tail->next = node; else head = node;
+        tail = node;
+        ++size_of_list;
     }
-}
 
-template<typename T>
-void list<T>::assign(const size_t size, const T &value)
-{
-    storage = new T[size];
-    size_of_list = size;
-    for (size_t i = 0; i < size; i++)
-    {
-        storage[i] = value;
+    void push_front(const T& value) {
+        Node* node = new Node(value, nullptr, head);
+        if (head) head->prev = node; else tail = node;
+        head = node;
+        ++size_of_list;
     }
-}
 
-template<typename T>
-T & list<T>::back() const
-{
-    if (size_of_list == 0)
-    {
-        throw std::out_of_range("Array is empty");
+    void pop_back() {
+        if (!tail) throw std::out_of_range("empty");
+        const Node* old = tail;
+        tail = tail->prev;
+        if (tail) tail->next = nullptr; else head = nullptr;
+        delete old;
+        --size_of_list;
     }
-    return *(storage + size_of_list - 1);
-}
 
-template<typename T>
-T * list<T>::begin() const
-{
-    return storage;
-}
-
-template<typename T>
-const T * list<T>::cbegin() const noexcept
-{
-    return storage;
-}
-
-template<typename T>
-const T * list<T>::cend() const noexcept
-{
-    return storage + size_of_list;
-}
-
-template<typename T>
-void list<T>::clear()
-{
-    delete[] storage;
-    storage = nullptr;
-    size_of_list = 0;
-}
-
-template<typename T>
-const T & list<T>::crbegin() const noexcept
-{
-    return storage + size_of_list;
-}
-
-template<typename T>
-const T * list<T>::crend() const noexcept
-{
-    return storage;
-}
-
-template<typename T>
-T list<T>::emplace(size_t pos, const T &value)
-{
-    if (pos >= size_of_list)
-    {
-        throw std::out_of_range("Array is empty");
+    void pop_front() {
+        if (!head) throw std::out_of_range("empty");
+        Node* old = head;
+        head = head->next;
+        if (head) head->prev = nullptr; else tail = nullptr;
+        delete old;
+        --size_of_list;
     }
-    storage[pos] = value;
-    return storage[pos];
-}
 
-template<typename T>
-list<T>::~list()
-{
-    if (storage != nullptr)
-    {
-        delete[] storage;
-        storage = nullptr;
+    bool empty() const noexcept { return size_of_list == 0; }
+    size_t size() const noexcept { return size_of_list; }
 
+    T& front() {
+        if (!head) throw std::out_of_range("empty");
+        return head->value;
+    }
+    T& back() {
+        if (!tail) throw std::out_of_range("empty");
+        return tail->value;
+    }
+
+    void clear() {
+        Node* p = head;
+        while (p) {
+            Node* next = p->next;
+            delete p;
+            p = next;
+        }
+        head = tail = nullptr;
         size_of_list = 0;
     }
-}
-
-
-
-
-
+};
