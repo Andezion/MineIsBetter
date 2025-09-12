04126bec90363aad5eec45e040fcdefea58a53a1
Andezion <245122@edu.p.lodz.pl>
2025-09-12 22:31:03 +0200

    added some funcs

--- algorithm.h
+++ algorithm.h
@@ -26,3 +26,40 @@ std::pair <const T&,const T&> minmax (const T& a, const T& b)
     }
     return std::make_pair(a, b);
 }
+
+template <class ForwardIterator> ForwardIterator min_element (ForwardIterator first, ForwardIterator last)
+{
+    if (first == last)
+    {
+        return last;
+    }
+
+    ForwardIterator smallest = first;
+
+    while (++first != last)
+    {
+        if (*first < *smallest)
+        {
+            smallest = first;
+        }
+    }
+
+    return smallest;
+}
+
+template <class ForwardIterator, class Compare> ForwardIterator min_element (ForwardIterator first, ForwardIterator last, Compare comp)
+{
+    if (first == last)
+    {
+        return last;
+    }
+    ForwardIterator smallest = first;
+    while (++first != last)
+    {
+        if (comp(*first, *smallest))
+        {
+            smallest = first;
+        }
+    }
+    return smallest;
+}
