77ce8c32b7eca8ca18b78874bd16a3ae0ecd18f5
Andezion <245122@edu.p.lodz.pl>
2025-09-23 22:49:14 +0200

    added new stuff to array

--- array.h
+++ array.h
@@ -26,6 +26,18 @@ public:
 
     ~array();
 
+    using value_type = T;
+    using reference = value_type &;
+    using const_reference = const value_type &;
+    using pointer = value_type *;
+    using const_pointer = const value_type *;
+    using iterator = value_type *;
+    using const_iterator = const value_type *;
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+    using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
+
     T& at(size_t index) const;
     T& back() const;
     T* begin() const noexcept;
