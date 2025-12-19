78930362ceb20f34896ab91f7264a483cf3d7eb5
Andezion <245122@edu.p.lodz.pl>
2025-12-19 21:14:25 +0100

    added new cool stuff

---
+++ dynamic_bitset.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <vector>
+#include <cstdint>
+#include <cstddef>
+
+class dynamic_bitset {
+public:
+    dynamic_bitset() = default;
+    explicit dynamic_bitset(std::size_t n): bits_((n+63)/64) {}
+    void resize(std::size_t n) { bits_.resize((n+63)/64); }
+    void set(std::size_t i) { bits_[i/64] |= (uint64_t(1) << (i%64)); }
+    void reset(std::size_t i) { bits_[i/64] &= ~(uint64_t(1) << (i%64)); }
+    bool test(std::size_t i) const { return bits_[i/64] & (uint64_t(1) << (i%64)); }
+private:
+    std::vector<uint64_t> bits_;
+};
