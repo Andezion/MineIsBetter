5cd8072b779c03b84b6c56523c8720ff36e4ff2a
Andezion <245122@edu.p.lodz.pl>
2025-12-19 21:18:40 +0100

    added new stuff

---
+++ segment_tree.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <vector>
+#include <functional>
+
+template<typename T>
+class segment_tree {
+public:
+    segment_tree() = default;
+    segment_tree(const std::vector<T>&) {}
+    
+};
