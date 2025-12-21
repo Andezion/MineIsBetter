582f640f80469a6fb8aaa579f02810fb48870feb
Andezion <245122@edu.p.lodz.pl>
2025-12-21 20:20:46 +0100

    cleaning up

--- skip_list.h
+++ skip_list.h
@@ -1,8 +1,36 @@
 #pragma once
 
+#include <vector>
+
 template<typename K, typename V>
-class skip_list {
+class skip_list 
+{
 public:
-    void insert(const K&, const V&) {}
-    V* find(const K&) { return nullptr; }
+    void insert(const K& k, const V& v) 
+    {
+        for (auto& kv : data_) 
+        {
+            if (kv.first == k) 
+            { 
+                kv.second = v; 
+                return; 
+            }
+        }
+        data_.emplace_back(k, v);
+    }
+
+    V* find(const K& k) 
+    {
+        for (auto& kv : data_) 
+        {
+            if (kv.first == k) 
+            {
+                return &kv.second;
+            }
+        }
+        return nullptr;
+    }
+
+private:
+    std::vector<std::pair<K,V>> data_;
 };
