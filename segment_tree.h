ff8c5616b39dad6a417c2ba596b12f3823cbd3d2
Andezion <245122@edu.p.lodz.pl>
2025-12-21 20:21:22 +0100

    cleaning up

--- segment_tree.h
+++ segment_tree.h
@@ -3,9 +3,32 @@
 #include <functional>
 
 template<typename T>
-class segment_tree {
+class segment_tree 
+{
 public:
     segment_tree() = default;
-    segment_tree(const std::vector<T>&) {}
+    segment_tree(const std::vector<T>& a) 
+    {
+        n_ = 1;
+        while (n_ < a.size()) 
+        {
+            n_ <<= 1;
+        }
+
+        tree_.assign(2*n_, T{});
+
+        for (size_t i = 0; i < a.size(); ++i) 
+        {
+            tree_[n_ + i] = a[i];
+        }
+        for (size_t i = n_ - 1; i >= 1; --i) 
+        {
+            tree_[i] = tree_[i<<1] + tree_[(i<<1)|1];
+            if (i == 1) 
+            {
+                break;
+            }
+        }
+    }
     
 };
