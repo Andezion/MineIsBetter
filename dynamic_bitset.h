1ed1f97bdaa16ef6c93943f30353b96b66d33796
Andezion <245122@edu.p.lodz.pl>
2025-12-24 14:05:53 +0100

    cleaned up

--- dynamic_bitset.h
+++ dynamic_bitset.h
@@ -3,14 +3,28 @@
 #include <cstdint>
 #include <cstddef>
 
-class dynamic_bitset {
+class dynamic_bitset
+{
 public:
     dynamic_bitset() = default;
-    explicit dynamic_bitset(std::size_t n): bits_((n+63)/64) {}
-    void resize(std::size_t n) { bits_.resize((n+63)/64); }
-    void set(std::size_t i) { bits_[i/64] |= (uint64_t(1) << (i%64)); }
-    void reset(std::size_t i) { bits_[i/64] &= ~(uint64_t(1) << (i%64)); }
-    bool test(std::size_t i) const { return bits_[i/64] & (uint64_t(1) << (i%64)); }
+    explicit dynamic_bitset(const std::size_t n): bits_((n + 63) / 64) {}
+
+    void resize(const std::size_t n)
+    {
+        bits_.resize((n + 63) / 64);
+    }
+    void set(const std::size_t i)
+    {
+        bits_[i / 64] |= static_cast<uint64_t>(1) << i % 64;
+    }
+    void reset(const std::size_t i)
+    {
+        bits_[i / 64] &= ~(static_cast<uint64_t>(1) << i % 64);
+    }
+    [[nodiscard]] bool test(const std::size_t i) const
+    {
+        return bits_[i / 64] & static_cast<uint64_t>(1) << i % 64;
+    }
 private:
     std::vector<uint64_t> bits_;
 };
