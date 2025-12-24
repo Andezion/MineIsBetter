d372864daf2470160d4dbdeeb01abbeed8e14634
Andezion <245122@edu.p.lodz.pl>
2025-12-24 14:31:16 +0100

    cleaned up

--- sparse_set.h
+++ sparse_set.h
@@ -1,5 +1,18 @@
 #pragma once
+
 #include <unordered_set>
+#include <vector>
 
 template<typename T>
-using sparse_set = std::unordered_set<T>;
+class sparse_set {
+public:
+	bool insert(const T& v) { return data_.insert(v).second; }
+	bool contains(const T& v) const { return data_.count(v) != 0; }
+	bool erase(const T& v) { return data_.erase(v) != 0; }
+	void clear() noexcept { data_.clear(); }
+	size_t size() const noexcept { return data_.size(); }
+	std::vector<T> values() const { return std::vector<T>(data_.begin(), data_.end()); }
+
+private:
+	std::unordered_set<T> data_;
+};
