677949da49f9318484731912e83ed1d7e31a40f3
Andezion <245122@edu.p.lodz.pl>
2025-12-24 14:07:06 +0100

    cleaned up

--- flat_map.h
+++ flat_map.h
@@ -3,15 +3,29 @@
 #include <algorithm>
 
 template<typename K, typename V>
-class flat_map {
+class flat_map
+{
 public:
-    void insert(const K& k, const V& v) {
+    void insert(const K& k, const V& v)
+    {
         data_.emplace_back(k,v);
-        std::sort(data_.begin(), data_.end(), [](auto &a, auto &b){ return a.first < b.first; });
+        std::sort(data_.begin(), data_.end(), [](auto &a, auto &b)
+        {
+            return a.first < b.first;
+        });
     }
-    V* find(const K& k) {
-        auto it = std::lower_bound(data_.begin(), data_.end(), k, [](auto &a, const K& val){ return a.first < val; });
-        if (it!=data_.end() && it->first==k) return &it->second; return nullptr;
+
+    V* find(const K& k)
+    {
+        auto it = std::lower_bound(data_.begin(), data_.end(), k, [](auto &a, const K& val)
+        {
+            return a.first < val;
+        });
+        if (it != data_.end() && it->first==k)
+        {
+            return &it->second;
+        }
+        return nullptr;
     }
 private:
     std::vector<std::pair<K,V>> data_;
