af5727045a68395521e5736851053e17a5300010
Andezion <245122@edu.p.lodz.pl>
2025-12-24 14:25:25 +0100

    cleaned up

--- segment_tree.h
+++ segment_tree.h
@@ -7,28 +7,29 @@ class segment_tree
 {
 public:
     segment_tree() = default;
-    segment_tree(const std::vector<T>& a) 
+    ~segment_tree() = default;
+
+    explicit segment_tree(const std::vector<T>& a)
     {
-        n_ = 1;
-        while (n_ < a.size()) 
+        int n_ = 1;
+        while (n_ < a.size())
         {
             n_ <<= 1;
         }
 
-        tree_.assign(2*n_, T{});
+        tree_.assign(2 * n_, T{});
 
-        for (size_t i = 0; i < a.size(); ++i) 
+        for (size_t i = 0; i < a.size(); ++i)
         {
             tree_[n_ + i] = a[i];
         }
-        for (size_t i = n_ - 1; i >= 1; --i) 
+        for (size_t i = n_ - 1; i >= 1; --i)
         {
-            tree_[i] = tree_[i<<1] + tree_[(i<<1)|1];
+            tree_[i] = tree_[i << 1] + tree_[i << 1 | 1];
             if (i == 1) 
             {
                 break;
             }
         }
     }
-    
 };
