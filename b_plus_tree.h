19132244c53f9aa56218a3afc142c71ffed661d1
Andezion <245122@edu.p.lodz.pl>
2025-12-24 13:47:03 +0100

    fixed

--- b_plus_tree.h
+++ b_plus_tree.h
@@ -3,12 +3,22 @@
 #include <map>
 
 template<typename K, typename V>
-class b_plus_tree {
+class b_plus_tree 
+{
 public:
-    void insert(const K& k, const V& v) { data_[k] = v; }
-    V* find(const K& k) {
+    void insert(const K& k, const V& v) 
+    { 
+        data_[k] = v; 
+    }
+
+    V* find(const K& k) 
+    {
         auto it = data_.find(k);
-        if (it == data_.end()) return nullptr;
+        
+        if (it == data_.end()) 
+        {
+            return nullptr;
+        }
         return &it->second;
     }
 
