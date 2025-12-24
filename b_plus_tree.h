c422f7cfa581d2d1aa53b3bae99a132c67e4ee4a
Andezion <245122@edu.p.lodz.pl>
2025-12-24 13:44:01 +0100

    added new cool tree

--- b_plus_tree.h
+++ b_plus_tree.h
@@ -1,8 +1,17 @@
 #pragma once
 
+#include <map>
+
 template<typename K, typename V>
 class b_plus_tree {
 public:
-    void insert(const K&, const V&) {}
-    V* find(const K&) { return nullptr; }
+    void insert(const K& k, const V& v) { data_[k] = v; }
+    V* find(const K& k) {
+        auto it = data_.find(k);
+        if (it == data_.end()) return nullptr;
+        return &it->second;
+    }
+
+private:
+    std::map<K,V> data_;
 };
