a0c31dc68e31e5c0f2af1fd15285a29038a05fc9
Andezion <245122@edu.p.lodz.pl>
2025-12-24 14:22:41 +0100

    cleaned up

--- ring_buffer.h
+++ ring_buffer.h
@@ -4,25 +4,47 @@
 #include <stdexcept>
 
 template<typename T>
-class ring_buffer {
+class ring_buffer
+{
 public:
     explicit ring_buffer(std::size_t capacity): buf_(capacity), head_(0), tail_(0), full_(false) {}
-    void push(const T& v) {
+    void push(const T& v)
+    {
         buf_[tail_] = v;
         tail_ = (tail_ + 1) % buf_.size();
-        if (full_) head_ = (head_ + 1) % buf_.size();
+
+        if (full_)
+        {
+            head_ = (head_ + 1) % buf_.size();
+        }
         full_ = tail_ == head_;
     }
-    T pop() {
-        if (empty()) throw std::out_of_range("empty");
+
+    T pop()
+    {
+        if (empty())
+        {
+            throw std::out_of_range("empty");
+        }
+
         T v = buf_[head_];
         head_ = (head_ + 1) % buf_.size();
         full_ = false;
         return v;
     }
-    bool empty() const noexcept { return (!full_ && head_==tail_); }
-    bool full() const noexcept { return full_; }
-    std::size_t capacity() const noexcept { return buf_.size(); }
+
+    [[nodiscard]] bool empty() const noexcept
+    {
+        return !full_ && head_==tail_;
+    }
+    [[nodiscard]] bool full() const noexcept
+    {
+        return full_;
+    }
+    [[nodiscard]] std::size_t capacity() const noexcept
+    {
+        return buf_.size();
+    }
 private:
     std::vector<T> buf_;
     std::size_t head_, tail_;
