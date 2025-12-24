e61803d32ebdd36b7a9db3468ac18c471e1ff375
Andezion <245122@edu.p.lodz.pl>
2025-12-24 14:21:20 +0100

    added new stuff

--- priority_queue.h
+++ priority_queue.h
@@ -3,12 +3,29 @@
 #include <algorithm>
 
 template<typename T>
-class priority_queue {
+class priority_queue
+{
 public:
-    void push(const T& v) { data_.push_back(v); std::push_heap(data_.begin(), data_.end()); }
-    void pop() { std::pop_heap(data_.begin(), data_.end()); data_.pop_back(); }
-    const T& top() const { return data_.front(); }
-    bool empty() const { return data_.empty(); }
+    void push(const T& v)
+    {
+        data_.push_back(v);
+        std::push_heap(data_.begin(), data_.end());
+    }
+
+    void pop()
+    {
+        std::pop_heap(data_.begin(), data_.end()); data_.pop_back();
+    }
+
+    const T& top() const
+    {
+        return data_.front();
+    }
+
+    [[nodiscard]] bool empty() const
+    {
+        return data_.empty();
+    }
 private:
     std::vector<T> data_;
 };
