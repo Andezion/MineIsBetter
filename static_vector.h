4f6b78971088e3c27a11b41930a6bdd68bc573f5
Andezion <245122@edu.p.lodz.pl>
2025-12-24 14:36:20 +0100

    lol

--- static_vector.h
+++ static_vector.h
@@ -4,22 +4,57 @@
 #include <stdexcept>
 
 template<typename T, std::size_t Capacity>
-class static_vector {
+class static_vector
+{
 public:
     static_vector() : sz_(0) {}
-    std::size_t size() const noexcept { return sz_; }
-    constexpr std::size_t capacity() const noexcept { return Capacity; }
-    void push_back(const T& v) {
-        if (sz_ >= Capacity) throw std::out_of_range("static_vector overflow");
+
+    [[nodiscard]] std::size_t size() const noexcept
+    {
+        return sz_;
+    }
+
+    static constexpr std::size_t capacity() noexcept
+    {
+        return Capacity;
+    }
+
+    void push_back(const T& v)
+    {
+        if (sz_ >= Capacity)
+        {
+            throw std::out_of_range("static_vector overflow");
+        }
         new(&data_[sz_]) T(v);
         ++sz_;
     }
-    void pop_back() { if (sz_) { --sz_; get_at(sz_).~T(); } }
-    T& operator[](std::size_t i) { return get_at(i); }
-    const T& operator[](std::size_t i) const { return get_at(i); }
+
+    void pop_back()
+    {
+        if (sz_)
+        {
+            --sz_;
+            get_at(sz_).~T();
+        }
+    }
+
+    T& operator[](const std::size_t i)
+    {
+        return get_at(i);
+    }
+    const T& operator[](const std::size_t i) const
+    {
+        return get_at(i);
+    }
 private:
-    alignas(T) unsigned char data_[sizeof(T)*Capacity];
+    alignas(T) unsigned char data_[sizeof(T)*Capacity]{};
     std::size_t sz_;
-    T& get_at(std::size_t i) { return *reinterpret_cast<T*>(data_ + sizeof(T)*i); }
-    const T& get_at(std::size_t i) const { return *reinterpret_cast<const T*>(data_ + sizeof(T)*i); }
+    T& get_at(const std::size_t i)
+    {
+        return *reinterpret_cast<T*>(data_ + sizeof(T)*i);
+    }
+    const T& get_at(const std::size_t i) const
+    {
+        return *reinterpret_cast<const T*>(data_ + sizeof(T)*i);
+    }
 };
