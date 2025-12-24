949d0de503eb739e61d9de84e32ee4a64fe30d02
Andezion <245122@edu.p.lodz.pl>
2025-12-24 14:23:50 +0100

    cleaned up

--- rope.h
+++ rope.h
@@ -4,10 +4,13 @@
 #include <memory>
 #include <utility>
 
-class rope {
-    struct Node {
+class rope
+{
+    struct Node
+    {
         std::shared_ptr<Node> left;
         std::shared_ptr<Node> right;
+
         std::string s;
         size_t len = 0;
 
@@ -15,31 +18,51 @@ class rope {
         Node(std::shared_ptr<Node> l, std::shared_ptr<Node> r) : left(std::move(l)), right(std::move(r))
         {
             len = 0;
-            if (left) len += left->len;
-            if (right) len += right->len;
+            if (left)
+            {
+                len += left->len;
+            }
+            if (right)
+            {
+                len += right->len;
+            }
         }
     };
 
 public:
     rope() = default;
-    rope(const std::string& s) : root_(std::make_shared<Node>(s)) {}
+    explicit rope(const std::string& s) : root_(std::make_shared<Node>(s)) {}
 
     rope& append(const rope& r)
     {
-        if (!root_) { root_ = r.root_; return *this; }
-        if (!r.root_) return *this;
+        if (!root_)
+        {
+            root_ = r.root_; return *this;
+        }
+        if (!r.root_)
+        {
+            return *this;
+        }
         root_ = std::make_shared<Node>(root_, r.root_);
         return *this;
     }
 
-    size_t size() const { return root_ ? root_->len : 0; }
+    [[nodiscard]] size_t size() const
+    {
+        return root_ ? root_->len : 0;
+    }
 
-    std::string str() const
+    [[nodiscard]] std::string str() const
     {
         std::string out;
-        if (!root_) return out;
+        if (!root_)
+        {
+            return out;
+        }
+
         out.reserve(root_->len);
         append_to_string(root_, out);
+
         return out;
     }
 
@@ -50,13 +73,23 @@ public:
         return r;
     }
 
-    rope& operator+=(const rope& other) { return append(other); }
+    rope& operator+=(const rope& other)
+    {
+        return append(other);
+    }
 
 private:
     void append_to_string(const std::shared_ptr<Node>& n, std::string& out) const
     {
-        if (!n) return;
-        if (!n->left && !n->right) { out += n->s; return; }
+        if (!n)
+        {
+            return;
+        }
+        if (!n->left && !n->right)
+        {
+            out += n->s; return;
+        }
+
         append_to_string(n->left, out);
         append_to_string(n->right, out);
     }
