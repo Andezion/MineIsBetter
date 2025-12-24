89d48a679ab9006490f00c578d9c7defcdbeeb0f
Andezion <245122@edu.p.lodz.pl>
2025-12-24 14:06:01 +0100

    cleaned up

--- fenwick_tree.h
+++ fenwick_tree.h
@@ -3,14 +3,25 @@
 #include <cstddef>
 
 template<typename T>
-class fenwick_tree {
+class fenwick_tree
+{
 public:
-    explicit fenwick_tree(std::size_t n): n_(n), data_(n+1) {}
-    void add(std::size_t i, T delta) {
-        for (++i; i <= n_; i += i & -i) data_[i] += delta;
+    explicit fenwick_tree(const std::size_t n): n_(n), data_(n+1) {}
+    void add(std::size_t i, T delta)
+    {
+        for (++i; i <= n_; i += i & -i)
+        {
+            data_[i] += delta;
+        }
     }
-    T sum(std::size_t i) const {
-        T r{}; for (++i; i; i -= i & -i) r += data_[i]; return r;
+    T sum(std::size_t i) const
+    {
+        T r{};
+        for (++i; i; i -= i & -i)
+        {
+            r += data_[i];
+        }
+        return r;
     }
 private:
     std::size_t n_;
