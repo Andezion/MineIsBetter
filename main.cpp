f4941316ac6d198fb0c4ba7a47b4d92282fd05e9
Andezion <245122@edu.p.lodz.pl>
2025-12-24 14:16:03 +0100

    added new stuff

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include "avl_tree.h"
 #include "red_black_tree.h"
 #include "b_plus_tree.h"
+#include "sparse_vector.h"
+#include "sparse_set.h"
+#include "rope.h"
 
 int main()
 {
@@ -21,6 +24,19 @@ int main()
     b.insert(10, "ten");
     b.insert(20, "twenty");
     if (auto s = b.find(20)) std::cout << "B+ 20: " << *s << "\n";
+
+    sparse_vector<int, std::string> sv;
+    sv[5] = "five";
+    if (sv.contains(5)) std::cout << "sparse_vector[5]: " << *sv.get(5) << "\n";
+
+    sparse_set<int> ss;
+    ss.insert(3);
+    if (ss.contains(3)) std::cout << "sparse_set contains 3\n";
+
+    rope r1("hello");
+    rope r2(" world");
+    r1.append(r2);
+    std::cout << "rope: " << r1.str() << " (len=" << r1.size() << ")\n";
 
     return 0;
 }
