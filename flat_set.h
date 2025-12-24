0249e37e4bdbf84fb0e02a6b99466df8f354059d
Andezion <245122@edu.p.lodz.pl>
2025-12-24 14:07:13 +0100

    cleaned up

--- flat_set.h
+++ flat_set.h
@@ -3,10 +3,18 @@
 #include <algorithm>
 
 template<typename T>
-class flat_set {
+class flat_set
+{
 public:
-    void insert(const T& v) { data_.push_back(v); std::sort(data_.begin(), data_.end()); }
-    bool contains(const T& v) const { return std::binary_search(data_.begin(), data_.end(), v); }
+    void insert(const T& v)
+    {
+        data_.push_back(v);
+        std::sort(data_.begin(), data_.end());
+    }
+    bool contains(const T& v) const
+    {
+        return std::binary_search(data_.begin(), data_.end(), v);
+    }
 private:
     std::vector<T> data_;
 };
