9e630c136e7327a172d1bc77f7051b4656321e59
Andezion <245122@edu.p.lodz.pl>
2025-12-27 12:19:26 +0100

    added new stuff

--- unique_ptr.h
+++ unique_ptr.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <utility>
-
 template<typename T>
 class unique_ptr
 {
@@ -10,12 +8,19 @@ class unique_ptr
 public:
     unique_ptr() noexcept = default;
     explicit unique_ptr(T* p) noexcept : ptr_(p) {}
-    ~unique_ptr() { delete ptr_; }
+    ~unique_ptr()
+    {
+        delete ptr_;
+    }
 
     unique_ptr(const unique_ptr&) = delete;
     unique_ptr& operator=(const unique_ptr&) = delete;
 
-    unique_ptr(unique_ptr&& o) noexcept : ptr_(o.ptr_) { o.ptr_ = nullptr; }
+    unique_ptr(unique_ptr&& o) noexcept : ptr_(o.ptr_)
+    {
+        o.ptr_ = nullptr;
+    }
+
     unique_ptr& operator=(unique_ptr&& o) noexcept
     {
         if (this != &o)
@@ -25,11 +30,33 @@ public:
         return *this;
     }
 
-    T* get() const noexcept { return ptr_; }
-    T* release() noexcept { T* p = ptr_; ptr_ = nullptr; return p; }
-    void reset(T* p = nullptr) noexcept { delete ptr_; ptr_ = p; }
+    T* get() const noexcept
+    {
+        return ptr_;
+    }
 
-    T& operator*() const noexcept { return *ptr_; }
-    T* operator->() const noexcept { return ptr_; }
-    explicit operator bool() const noexcept { return ptr_ != nullptr; }
+    T* release() noexcept
+    {
+        T* p = ptr_;
+        ptr_ = nullptr;
+        return p;
+    }
+
+    void reset(T* p = nullptr) noexcept
+    {
+        delete ptr_; ptr_ = p;
+    }
+
+    T& operator*() const noexcept
+    {
+        return *ptr_;
+    }
+    T* operator->() const noexcept
+    {
+        return ptr_;
+    }
+    explicit operator bool() const noexcept
+    {
+        return ptr_ != nullptr;
+    }
 };
